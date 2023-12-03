c9ab5f92a476a81c9a1c18179898bf825f9992f3
chrisscp <c.cassamajorp@gmail.com>
2023-12-03 17:43:55 -0500

    erblnerblkns

---
+++ src/allseaing_programs/pStationKeeping/StationKeeping.h
@@ -0,0 +1,41 @@
+/************************************************************/
+/*    NAME: Christian and Janelle                                              */
+/*    ORGN: MIT, Cambridge MA                               */
+/*    FILE: StationKeeping.h                                          */
+/*    DATE: December 29th, 1963                             */
+/************************************************************/
+
+#ifndef StationKeeping_HEADER
+#define StationKeeping_HEADER
+
+#include "MOOS/libMOOS/Thirdparty/AppCasting/AppCastingMOOSApp.h"
+
+class StationKeeping : public AppCastingMOOSApp
+{
+ public:
+   StationKeeping();
+   ~StationKeeping();
+
+ protected: // Standard MOOSApp functions to overload  
+   bool OnNewMail(MOOSMSG_LIST &NewMail);
+   bool Iterate();
+   bool OnConnectToServer();
+   bool OnStartUp();
+
+ protected: // Standard AppCastingMOOSApp function to overload 
+   bool buildReport();
+
+ protected:
+   void registerVariables();
+
+ private: // Configuration variables
+   bool   m_first_reading;
+   double m_current_x;
+   double m_current_y;
+   double m_original_x;
+   double m_original_y;
+
+ private: // State variables
+};
+
+#endif 
---
+++ src/allseaing_programs/pStationKeeping/StationKeeping_Info.h
@@ -0,0 +1,18 @@
+/****************************************************************/
+/*   NAME: Christian and Janelle                                             */
+/*   ORGN: MIT, Cambridge MA                                    */
+/*   FILE: StationKeeping_Info.h                                      */
+/*   DATE: Dececmber 29th, 1963                                 */
+/****************************************************************/
+
+#ifndef StationKeeping_INFO_HEADER
+#define StationKeeping_INFO_HEADER
+
+void showSynopsis();
+void showHelpAndExit();
+void showExampleConfigAndExit();
+void showInterfaceAndExit();
+void showReleaseInfoAndExit();
+
+#endif
+
