adc73dcbef37c65aff778e539e626a5b98d4d4a6
Toya Takahashi <toyatakahashi522@gmail.com>
2024-04-17 23:53:18 -0400

    updated missions

--- missions/all_seaing/launch_shoreside.sh
+++ missions/all_seaing/launch_shoreside.sh
@@ -1,36 +1,20 @@
 #!/bin/bash -e
 WARP=1
 VNAME="fishnships"
-VPORT="9000"
+VEHICLE_IP="192.168.1.3"
+MOOS_PORT="9000"
+PSHARE_PORT="9832"
 
-#----------------------------------------------------------
-#  Part 1: Check for and handle command-line arguments
-#----------------------------------------------------------
-for ARGI; do
-    if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
-        echo "launch.sh [SWITCHES] [time_warp]   "
-        echo "  --help, -h           Show this help message            " 
-        exit 0;
-    elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$WARP" = 1 ]; then 
-        WARP=$ARGI
-    else 
-        echo "Bad argument:" $ARGI " Exiting with code: 1"
-        exit 1
-    fi
-done
-
-#-------------------------------------------------------
-#  Part 2: Create the .moos file
-#-------------------------------------------------------
+# Create the .moos file
 printf "Preparing shoreside .moos file...\n"
 nsplug meta_shoreside.moos targ_shoreside.moos -f \
     WARP=$WARP                                    \
     VNAME=$VNAME                                  \
-    VPORT=$VPORT
+    VEHICLE_IP=$VEHICLE_IP                        \
+    MOOS_PORT=$MOOS_PORT                          \
+    PSHARE_PORT=$PSHARE_PORT
 
-#-------------------------------------------------------
-#  Part 3: Launch the processes
-#-------------------------------------------------------
+# Launch the processes
 echo "Launching shoreside MOOS Community"
 pAntler targ_shoreside.moos >& /dev/null &
 uMAC -t targ_shoreside.moos
--- missions/test_share/launch_shoreside.sh
+++ missions/test_share/launch_shoreside.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+WARP=1
 VNAME="fishnships"
 VEHICLE_IP="192.168.1.4"
 MOOS_PORT="9000"
@@ -7,6 +8,7 @@ PSHARE_PORT="9832"
 # Create the .moos file
 printf "Preparing shoreside .moos file...\n"
 nsplug meta_shoreside.moos targ_shoreside.moos -f \
+    WARP=$WARP                                    \
     VNAME=$VNAME                                  \
     VEHICLE_IP=$VEHICLE_IP                        \
     MOOS_PORT=$MOOS_PORT                          \
