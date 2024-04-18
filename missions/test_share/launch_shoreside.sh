10489d28f9eb09d6a85dae0cf7625c54bad7a2fb
Toya Takahashi <toyatakahashi522@gmail.com>
2024-04-17 23:00:53 -0400

    final test moos

--- missions/test_share/launch_shoreside.sh
+++ missions/test_share/launch_shoreside.sh
@@ -1,42 +1,18 @@
 #!/bin/bash -e
-WARP=1
 VNAME="fishnships"
-IP_ADDR="localhost"
+VEHICLE_IP="192.168.1.4"
 MOOS_PORT="9000"
-PSHARE_PORT="9200"
+PSHARE_PORT="9832"
 
-#----------------------------------------------------------
-#  Part 1: Check for and handle command-line arguments
-#----------------------------------------------------------
-for ARGI; do
-    if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
-        echo "launch.sh [SWITCHES] [time_warp]   "
-        echo "  --help, -h           Show this help message            " 
-        exit 0;
-    elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$WARP" = 1 ]; then 
-        WARP=$ARGI
-    elif [ "${ARGI:0:5}" = "--ip=" ]; then
-        IP_ADDR="${ARGI#--ip=*}"
-    else 
-        echo "Bad argument:" $ARGI " Exiting with code: 1"
-        exit 1
-    fi
-done
-
-#-------------------------------------------------------
-#  Part 2: Create the .moos file
-#-------------------------------------------------------
+# Create the .moos file
 printf "Preparing shoreside .moos file...\n"
 nsplug meta_shoreside.moos targ_shoreside.moos -f \
-    WARP=$WARP                                    \
     VNAME=$VNAME                                  \
-    IP_ADDR=$IP_ADDR                              \
+    VEHICLE_IP=$VEHICLE_IP                        \
     MOOS_PORT=$MOOS_PORT                          \
     PSHARE_PORT=$PSHARE_PORT
 
-#-------------------------------------------------------
-#  Part 3: Launch the processes
-#-------------------------------------------------------
+# Launch the processes
 echo "Launching shoreside MOOS Community"
 pAntler targ_shoreside.moos >& /dev/null &
 uMAC -t targ_shoreside.moos
--- missions/test_share/launch_vehicle.sh
+++ missions/test_share/launch_vehicle.sh
@@ -1,36 +1,18 @@
 #!/bin/bash -e
-WARP=1
 VNAME="fishnships"
-VPORT="9001"
+SHORE_IP="192.168.1.3"
+MOOS_PORT="9001"
+PSHARE_PORT="9832"
 
-#----------------------------------------------------------
-#  Part 1: Check for and handle command-line arguments
-#----------------------------------------------------------
-for ARGI; do
-    if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
-        echo "launch.sh [SWITCHES] [time_warp]   "
-        echo "  --help, -h           Show this help message            " 
-        exit 0;
-    elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$WARP" = 1 ]; then 
-        WARP=$ARGI
-    else 
-        echo "Bad argument:" $ARGI " Exiting with code: 1"
-        exit 1
-    fi
-done
-
-#----------------------------------------------------------
-#  Part 2: Create the .moos and .bhv files
-#----------------------------------------------------------
+# Create the .moos file
 printf "Preparing vehicle .moos file...\n"
 nsplug meta_vehicle.moos targ_$VNAME.moos -f \
-    WARP=$WARP                               \
     VNAME=$VNAME                             \
-    VPORT=$VPORT                             
+    SHORE_IP=$SHORE_IP                       \
+    MOOS_PORT=$MOOS_PORT                     \
+    PSHARE_PORT=$PSHARE_PORT
 
-#----------------------------------------------------------
-#  Part 3: Launch the processes
-#----------------------------------------------------------
+# Launch the processes
 echo "Launching $VNAME MOOS Community"
 pAntler targ_$VNAME.moos >& /dev/null &
 uMAC -t targ_$VNAME.moos
